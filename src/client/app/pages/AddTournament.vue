fff0608d8e56a48c715c85a56ddbca804f0315c6
Ambra <ambra.isenberg@gmail.com>
2020-10-27 15:51:35 -0500

    added progress bar to add tournament page

--- src/client/app/pages/AddTournament.vue
+++ src/client/app/pages/AddTournament.vue
@@ -6,46 +6,73 @@
     <h1>
       <i class="fad fa-home-lg"></i> Add Tournament
     </h1>
-
-    <hr>
-
-    <div v-show="true">
-      <div>
-        <button v-if="canGoBack" @click="goBack">
-          <i class="fad fa-arrow-left"></i>
-          Go Back
-        </button>
-
-        <button v-else disabled>
-          <i class="fad fa-arrow-to-left"></i>
-          Go Back
-        </button>
-
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Current Step:&nbsp;{{currentStep}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-        <button v-if="canGoForward" @click="goForward">
-          <i class="fad fa-arrow-right"></i>
-          Go Forward
-        </button>
-
-        <button v-else disabled>
-          <i class="fad fa-arrow-to-right"></i>
-          Go Forward
-        </button>
-      </div>
+<div class="container">
+    <div v-if="currentStep === 1">
+      
+      <ul class="progressbar">
+          <li class="active">Name/Date</li>
+          <li>Location</li>
+          <li>Details</li>
+          <li>Registration</li>
+          <li>Finished</li>
+  </ul>
+</div>
+    
+    <div v-else-if="currentStep === 2">
+      
+      <ul class="progressbar">
+          <li class="active">Name/Date</li>
+          <li class="active">Location</li>
+          <li>Details</li>
+          <li>Registration</li>
+          <li>Finished</li>
+  </ul>
+</div>
+    
+    <div v-else-if="currentStep === 3">
+      
+      <ul class="progressbar">
+          <li class="active">Name/Date</li>
+          <li class="active">Location</li>
+          <li class="active">Details</li>
+          <li>Registration</li>
+          <li>Finished</li>
+  </ul>
+</div>
+   
+    <div v-else-if="currentStep === 4">
+      <ul class="progressbar">
+          <li class="active">Name/Date</li>
+          <li class="active">Location</li>
+          <li class="active">Details</li>
+          <li class="active">Registration</li>
+          <li>Finished</li>
+  </ul>
+</div>
+    <div v-else>
+    
+      <ul class="progressbar">
+          <li class="active">Name/Date</li>
+          <li class="active">Location</li>
+          <li class="active">Details</li>
+          <li class="active">Registration</li>
+          <li class="active">Finished</li>
+  </ul>
+</div>
     </div>
-
+<br>  
+<hr>
+    
     <hr>
-
-    <div v-show="currentStep === 1">
-      <p><input v-model="tournament.eventName" placeholder="Tournament Name"/> Tournament name is: {{tournament.eventName || "___"}}</p>
+<div class="regcard">
+      <div v-show="currentStep === 1">
+      <p><input v-model="tournament.eventName" placeholder="Tournament Name"/> Tournament name is: {{tournament.eventName || "___"}}</p><br>
       <p><input v-model="tournament.location.address" placeholder="Where"/> Where? {{tournament.location.address || "___"}} TODO: implement google maps</p>
     </div>
 
-
     <div v-show="currentStep === 2">
-      <p><input v-model="tournament.registration.numberOfMats" placeholder="Number of Mats"/> How Many Mats?  {{tournament.registration.numberOfMats || "___"}}</p>
-      <p><input v-model="tournament.registration.minWrestlers" placeholder="Min # of Wrestlers"/> {{tournament.registration.minWrestlers || "___"}} Min # of Wrestlers</p>
+      <p><input v-model="tournament.registration.numberOfMats" placeholder="Number of Mats"/> How Many Mats?  {{tournament.registration.numberOfMats || "___"}}</p><br>
+      <p><input v-model="tournament.registration.minWrestlers" placeholder="Min # of Wrestlers"/> {{tournament.registration.minWrestlers || "___"}} Min # of Wrestlers</p><br>
       <p><input v-model="tournament.registration.maxWrestlers" placeholder="Max # of Wrestlers"/> {{tournament.registration.maxWrestlers || "___"}} Max # of Wrestlers</p>
       <p><input v-model="tournament.registration.entryFee" placeholder="How much $"/> Entry Fee Cost is ${{tournament.registration.entryFee || "___"}}</p>
       <p><input v-model="tournament.registration.inviteOnly" placeholder="Invite Only?"/> Invite only? {{tournament.registration.inviteOnly || "___"}} (meaning only certain teams can register)</p>
@@ -66,8 +93,35 @@
       <h1>Review Data:</h1>
       <p>{{JSON.stringify(tournament, null, 2)}}</p>
       <button @click="addTournament()">click this button</button>
+    </div></div>
+<hr>
+    <div v-show="true" style="text-align: center">
+      <div>
+        <button v-if="canGoBack" @click="goBack">
+          <i class="fad fa-arrow-to-left"></i>
+          Go Back
+        </button>
+
+        <button v-else disabled>
+          <i class="fad fa-arrow-to-left"></i>
+          Go Back
+        </button>
+
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Current Step:&nbsp;{{currentStep}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+        <button v-if="canGoForward" @click="goForward">
+          Next Step
+          <i class="fad fa-arrow-to-right"></i>
+        </button>
+
+        <button v-else disabled>
+          Go Forward
+          <i class="fad fa-arrow-to-right"></i>
+        </button>
+      </div>
     </div>
 
+    <hr>
   </div>
 </template>
 
@@ -101,7 +155,7 @@ export default {
   },
   data: () => {
     return {
-      currentStep : 1
+      currentStep : 1,
     }
   },
   components: {
@@ -110,5 +164,64 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style scoped>
+.regcard {
+      width: 100%;
+      min-height: 200;
+      text-align: left;
+      padding: 20;}
+.container {
+      width: 100%;
+      margin: auto;
+      padding-bottom: 50;
+      }
+  .progressbar {
+      counter-reset: step 0;
+  }
+  .progressbar li {
+      list-style-type: none;
+      width: 20%;
+      float: left;
+      font-size: 14px;
+      position: relative;
+      text-align: center;
+      text-transform: uppercase;
+      color: #363636;
+  }
+  .progressbar li:before {
+      width: 30px;
+      height: 30px;
+      content: counter(step);
+      counter-increment: step;
+      line-height: 30px;
+      border: 3px solid #363636;
+      display: block;
+      text-align: center;
+      margin: 0 auto 10px auto;
+      border-radius: 50%;
+      background-color: white;
+  }
+  .progressbar li:after {
+      width: 100%;
+      height: 3px;
+      content: '';
+      position: absolute;
+      background-color: #363636;
+      top: 15px;
+      left: -50%;
+      z-index: -1;
+  }
+  .progressbar li:first-child:after {
+      content: none;
+  }
+  .progressbar li.active {
+      color: #363636;
+  }
+  .progressbar li.active:before {
+      border-color: #363636;
+      background-color: #1bb630  
+  }
+  .progressbar li.active:after {
+      background-color: #1bb630;
+  }
+  </style>
