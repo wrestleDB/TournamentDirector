3d79735eaff1d9546ea70e63d5a0ba56741b0fda
Aldon <aisenberg@freightview.com>
2020-11-25 13:19:33 -0600

    wire up backend with api server

--- src/client/app/pages/EventPage.vue
+++ src/client/app/pages/EventPage.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     <h1>Event Page </h1>
+    <p>So we have a list of tournaments, when the user clicks one of the tiles, they should be directed to a "dashboard" page that has all the tourney details for that specific tournament.</p>
+    <span> I say dashboard because we could list out</span>
+    <ul>
+      <li> Number of wrestlers entered </li>
+      <li> Entry Fee's collected </li>
+      <li> Countdown to tournament </li>
+      <li> teams entered, </li>
+      <li> wrestlers entered </li>
+      <li> Button to start weigh-ins (). </li>
+      <li> Button to start tournament (should be disabled until tournament start date, or force start). </li>
+      <li><a href="https://github.com/wrestleDB/TournamentDirector/issues/53" target="__blank">Github Issue</a></li>
+    </ul>
+
     <p>Data here: {{event}}</p>
   </div>
 </template>
